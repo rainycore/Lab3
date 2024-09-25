82933306d0dfb3adbf3fba30a1db9777535f0f7d
rainychen <rainy.chen@mail.utoronto.ca>
2024-09-25 16:47:04 -0400

    finish countrycodeconverter.java

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -39,9 +39,6 @@ public class JSONTranslationExample {
         return canada.getString("es");
     }
 
-    // TODO Task: Complete the method below to generalize the above to get the country name
-    //            for any country code and language code from sample.json.
-
     /**
      * Returns the name of the country based on the provided country and language codes.
      * @param countryCode the country, as its three-letter code.
@@ -49,6 +46,14 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+        int i;
+        for (i = 0; i < jsonArray.length(); i++) {
+            if (jsonArray.getJSONObject(i).getString("alpha3").equals(countryCode)) {
+                if (jsonArray.getJSONObject(i).keySet().contains(languageCode)) {
+                    return jsonArray.getJSONObject(i).getString(languageCode);
+                }
+            }
+        }
         return "Country not found";
     }
 
